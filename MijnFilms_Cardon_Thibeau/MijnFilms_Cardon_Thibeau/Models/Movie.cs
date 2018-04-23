f597b2fe63930603493db401d2c666bbbf6bd023
CardonThibeau <thibeau.cardon@student.howest.be>
2018-04-23 10:18:05 +0200

    prep

---
+++ MijnFilms_Cardon_Thibeau/MijnFilms_Cardon_Thibeau/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MijnFilms_Cardon_Thibeau.Models
+{
+    public class Movie
+    {
+        [Required]
+        public int MovieID { get; set; }
+
+        [Required]
+        public int Title { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public int GenreID { get; set; }
+
+        [Required]
+        public int DirectorID { get; set; }
+
+        [Required]
+        public string Stars { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+
+
+    }
+}
