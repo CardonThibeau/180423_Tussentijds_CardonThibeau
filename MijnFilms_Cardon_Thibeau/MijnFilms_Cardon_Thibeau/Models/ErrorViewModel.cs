ada714ebd99aea15be781bbffc4d242e1c593171
CardonThibeau <thibeau.cardon@student.howest.be>
2018-04-23 09:42:33 +0200

    first commit

---
+++ MijnFilms_Cardon_Thibeau/MijnFilms_Cardon_Thibeau/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MijnFilms_Cardon_Thibeau.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
